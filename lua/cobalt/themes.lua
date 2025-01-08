a82f866fc09b19aee096f56c649a5d3f3dba9e4d
Jacob Scott <jscott2718@gmail.com>
2025-01-08 23:55:15 +0000

    Update line length indicator colour

--- lua/cobalt/themes.lua
+++ lua/cobalt/themes.lua
@@ -100,7 +100,7 @@ return {
                 bg_m1      = palette.TangerineBurst, -- Status line, etc
 
                 bg         = palette.MarineBlue,     -- Normal background
-                bg_p1      = palette.PolarWhite,     -- Line length highlight
+                bg_p1      = palette.MidnightNavy,  -- Line length highlight
                 bg_p2      = palette.DeepAzure,      -- Current line highlight
 
                 special    = palette.PeachSherbet,
